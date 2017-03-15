78354502a2c2ee12135762daa881a39ed40d46e8
Dakota <bazookaharwell@gmail.com>
2017-03-15 13:47:29 -1000

    some methods

--- cash-register.js
+++ cash-register.js
@@ -1,6 +1,39 @@
 var calculatorScript = require( './calculator.js' );
-var calculator = calculatorScript.calculatorModule();
+var calculatorDollars = calculatorScript.calculatorModule();
+var calculatorCents = calculatorScript.calculatorModule();
 
-calculator.add(2);
-calculator.multiply(12);
-console.log(calculator.getTotal());
+var cashRegisterModule = (function (){
+
+  var _dollars = 0;
+  var _cents = 0;
+
+  function getDollars() {
+    return _dollars;
+  }
+
+  function getCents() {
+    return _cents;
+  }
+
+  function addMoney(element, operand) {
+    //TODO
+    var centsToAdd = operand - Math.floor(operand);
+    var dollarsToAdd = Math.floor(operand);
+  }
+
+  function decimal(element) {
+    if (element.innerHTML.search(/\./) === -1) {
+      element.innerHTML += ".";
+    }
+  }
+
+  return {
+    getDollars,
+    getCents,
+    addMoney,
+    decimal
+  }
+
+
+});
+
