a50d186a4a0fbfb2d4c74901e00219d9c654cc5b
Dakota <bazookaharwell@gmail.com>
2017-03-15 17:40:12 -1000

    fixed subtraction leading 0

--- cash-register.js
+++ cash-register.js
@@ -3,23 +3,43 @@ var cashRegisterModule = (function (){
 
   var _dollars = 0;
   var _cents = 0;
-  var _dollarsDisplay = 0;
-  var _centsDisplay = 0;
-  var _display = "$0.00";
-  var _displayElement = null;
 
   function getDollars() {
     return _dollars;
+  }
+
+  function setDollars(value) {
+    _dollars = value;
   }
 
   function getCents() {
     return _cents;
   }
 
+  function setCents(value) {
+    _cents = value;
+  }
+
+  function addMoney(dollars, cents) {
+    _dollars += dollars;
+    _cents += cents;
+    while (_cents >= 100) {
+      _dollars++;
+      cents -= 100;
+    }
+  }
+
+  function subtractMoney(dollars, cents) {
+
+  }
+
+
   return {
     getDollars,
+    setDollars,
     getCents,
-}
+    setCents
+};
 
 
 });
