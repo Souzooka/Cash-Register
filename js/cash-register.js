e3c2a88a8aad80006e3191988f4c354292259b96
Dakota <bazookaharwell@gmail.com>
2017-03-18 10:22:39 -1000

    ASCII text updating

--- js/cash-register.js
+++ js/cash-register.js
@@ -3,9 +3,47 @@ window.onload = function() {
   var calculator = calculatorModule();
   var calculatorFunction = null;
   var displayNum = 0;
+  var money = 0;
+  var moneyStr = "";
   var display = document.getElementById("registerDisplay");
   var decimal = false;
   var originalDisplayStr = "[____________________________]";
+
+  function updateASCIIDisplay(numStr) {
+
+    var numMultiplier;
+
+    moneyStr = display.innerHTML.slice(2);
+    moneyStr = String(parseFloat(moneyStr));
+    money = Number(moneyStr);
+
+    if (numStr && !decimal) {
+      moneyStr += numStr;
+    }
+
+    if (Number(moneyStr) >= 1e+20) {
+      alert("Buffer Overflow Error!");
+      money = 0;
+      moneyStr = "0";
+    }
+
+
+    moneyStr = Number(moneyStr).toFixed(2);
+
+
+    display.innerHTML = `[$${moneyStr}`;
+
+    // display.length should end with 30, we cap it with a "]"
+    while (display.innerHTML.length < 29) {
+      display.innerHTML += "_";
+    }
+    display.innerHTML += "]";
+
+  }
+
+  document.getElementById("button8").addEventListener("click", function(){
+    updateASCIIDisplay("8");
+  });
 
 
 /*if (calculator.recallMemory() === 0) {
