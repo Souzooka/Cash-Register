01ad88c8f7c50aa35dc27d4383b128c42c062dc3
Souzooka <bazookaharwell@gmail.com>
2017-03-19 19:09:31 -1000

    String filtering

--- js/cash-register.js
+++ js/cash-register.js
@@ -1,8 +1,26 @@
 window.onload = function() {
   "use strict";
   var emptyDisplay = true;
+  // we want to truncate display string if it gets too long
+  var displayString = "";
+  var calculationString = "";
+  var originalDisplayString = "[$0.00_______________________]";
+  var display = document.getElementById("registerDisplay");
   var number = document.querySelectorAll("span.btn-number");
   var operation = document.querySelectorAll("span.btn-number");
+
+
+  function addToDisplay(str) {
+    displayString = display.innerHTML.replace(/[\[\]_$'']/g," ").trim();
+  }
+
+  function updateDisplay() {
+
+  }
+
+  function clearDisplay() {
+
+  }
 
   for (let i = 0; i < number.length; i++) {
     number[i].addEventListener("click", function(){
@@ -10,7 +28,7 @@ window.onload = function() {
     });
   }
 
-  for (let i = 0; i < number.length; i++) {
+  for (let i = 0; i < operation.length; i++) {
     number[i].addEventListener("click", function(){
       addToDisplay(this.innerHTML.replace(/[\[\]'']/g,""));
     });
